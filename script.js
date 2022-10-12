3a719bfdece287aeb77ce8d3a5a637feb8cfb3f6
Joakim <joakim.andersson920202@gmail.com>
2022-10-12 10:20:51 +0200

    Add styleSheet to center divs and add hover effect. Add color to divs on mouse-input.

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
 const containerDiv = document.querySelector('#container');
-const arrayWithAllDivs = [];
+const containerDivEL = document.getElementById('container');
+const individualDivs = document.getElementsByClassName("singleDiv");
+var canPaint = false; 
+console.log(containerDivEL);
+
+
+
 
 
 for(let i = 1; i < 17; i++){
     const createDiv = document.createElement('div');
     containerDiv.appendChild(createDiv);
-    createDiv.setAttribute("id", i);
-    createDiv.className = "boardDivs";
+    createDiv.className = "divGroup";
+    createDiv.setAttribute("id", "row" + i)
 
-    arrayWithAllDivs[i] = createDiv;
+    for(let n = 1; n < 17; n++){
+        var rowDiv = document.createElement('div');
+        createDiv.appendChild(rowDiv);
+        rowDiv.classList.add("singleDiv");
+        rowDiv.setAttribute("onmouseover", "addColorClass(this)");
+              
+    }
+    
 }
 
-console.log(arrayWithAllDivs);
+function addColorClass(element){  
+    if(!canPaint) return;
+       element.classList.add("giveColorClass");
+       
+}
+
+containerDivEL.onmousedown = function () {canPaint = true; console.log(canPaint);}
+containerDivEL.onmouseup = function () {canPaint = false; console.log(canPaint);}
+
+
+
+
+
+
+
+
+
 
