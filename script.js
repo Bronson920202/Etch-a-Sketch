e9330049872ed31eccfd7d3244b3a108298c6038
Joakim <joakim.andersson920202@gmail.com>
2022-10-12 16:19:47 +0200

    display value of slider in sliderHeader

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 const containerDiv = document.querySelector('#container');
 const containerDivEL = document.getElementById('container');
-const individualDivs = document.getElementsByClassName("singleDiv");
+const individualDivs = document.getElementsByClassName('singleDiv');
+const sliderHeader = document.getElementById('sliderHeader');
+const slider = document.getElementById('slider');
+const sliderValue = slider.getAttribute('value');
+
+var resolution = sliderValue;
+
 var canPaint = false; 
-console.log(containerDivEL);
 
 
 
-
-
-for(let i = 1; i < 17; i++){
+for(let i = 1; i < resolution; i++){
     const createDiv = document.createElement('div');
     containerDiv.appendChild(createDiv);
     createDiv.className = "divGroup";
     createDiv.setAttribute("id", "row" + i)
 
-    for(let n = 1; n < 17; n++){
+    for(let n = 1; n < resolution; n++){
         var rowDiv = document.createElement('div');
         createDiv.appendChild(rowDiv);
         rowDiv.classList.add("singleDiv");
@@ -24,14 +27,23 @@ for(let i = 1; i < 17; i++){
     
 }
 
+sliderHeader.textContent = sliderValue.toString();
+
 function addColorClass(element){  
     if(!canPaint) return;
        element.classList.add("giveColorClass");
        
 }
 
+// function displayResolution(element, varibleToDisplay) {
+//     element.textContent = varibleToDisplay.toString;
+// }
+
+// slider.addEventListener('change', displayResolution(sliderHeader, resolution));
+
 containerDivEL.onmousedown = function () {canPaint = true; console.log(canPaint);}
 containerDivEL.onmouseup = function () {canPaint = false; console.log(canPaint);}
+containerDivEL.onmouseleave = function () {canPaint = false; console.log(canPaint);}
 
 
 
