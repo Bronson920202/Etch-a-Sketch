534d0d9a6636c5b530d2bc6b80e267cd6024b1b6
Joakim <joakim.andersson920202@gmail.com>
2022-10-17 08:41:34 +0200

    Add header to visualize resoultion choice, link value and slider, update canvas on slider button release

--- script.js
+++ script.js
@@ -1,33 +1,49 @@
-const containerDiv = document.querySelector('#container');
-const containerDivEL = document.getElementById('container');
-const individualDivs = document.getElementsByClassName('singleDiv');
-const sliderHeader = document.getElementById('sliderHeader');
-const slider = document.getElementById('slider');
-const sliderValue = slider.getAttribute('value');
 
-var resolution = sliderValue;
+var individualDivs = [];
+var rows = [];
+
+const containerDiv = document.querySelector('#container');
+
+const sliderHeader = document.querySelector('#sliderHeader');
+const slider = document.querySelector('#slider');
+var resolution;
 
 var canPaint = false; 
 
-
-
-for(let i = 1; i < resolution; i++){
-    const createDiv = document.createElement('div');
-    containerDiv.appendChild(createDiv);
-    createDiv.className = "divGroup";
-    createDiv.setAttribute("id", "row" + i)
-
-    for(let n = 1; n < resolution; n++){
-        var rowDiv = document.createElement('div');
-        createDiv.appendChild(rowDiv);
-        rowDiv.classList.add("singleDiv");
-        rowDiv.setAttribute("onmouseover", "addColorClass(this)");
-              
-    }
-    
+sliderHeader.textContent = slider.getAttribute('value');
+slider.oninput = function () {
+    sliderHeader.textContent = this.value;
+    resolution = this.value;
 }
 
-sliderHeader.textContent = sliderValue.toString();
+slider.addEventListener('click', generateCanvas);
+
+function generateCanvas() {    
+    if(rows.length > 0){
+            rows.forEach(element => element.remove());
+            individualDivs.forEach(element => element.remove());
+        }
+        
+    for(let i = 1; i < resolution; i++){
+        const createDiv = document.createElement('div');
+        containerDiv.appendChild(createDiv);
+        createDiv.className = "divGroup";
+        createDiv.setAttribute("id", "row" + i)
+    
+        for(let n = 1; n < resolution; n++){
+            var rowDiv = document.createElement('div');
+            createDiv.appendChild(rowDiv);
+            rowDiv.classList.add("singleDiv");
+            rowDiv.setAttribute("onmouseover", "addColorClass(this)");
+                  
+        }
+        
+    }
+    individualDivs = document.querySelectorAll('.singleDiv');
+    rows = document.querySelectorAll('.divGroup');
+    
+    
+}
 
 function addColorClass(element){  
     if(!canPaint) return;
@@ -35,15 +51,9 @@ function addColorClass(element){
        
 }
 
-// function displayResolution(element, varibleToDisplay) {
-//     element.textContent = varibleToDisplay.toString;
-// }
-
-// slider.addEventListener('change', displayResolution(sliderHeader, resolution));
-
-containerDivEL.onmousedown = function () {canPaint = true; console.log(canPaint);}
-containerDivEL.onmouseup = function () {canPaint = false; console.log(canPaint);}
-containerDivEL.onmouseleave = function () {canPaint = false; console.log(canPaint);}
+containerDiv.onmousedown = function () {canPaint = true; console.log(canPaint);}
+containerDiv.onmouseup = function () {canPaint = false; console.log(canPaint);}
+containerDiv.onmouseleave = function () {canPaint = false; console.log(canPaint);}
 
 
 
